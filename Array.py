cc308937cbad33530931a8cb05ed9ca266fbad10
Hihongboyang <245035255@qq.com>
2019-03-19 09:20:00 +0800

    Array class

---
+++ Array.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+
+class Array:
+    def __init__(self, capacity, fillValue=None):
+        self._items = list()
+        for count in range(capacity):
+            self._items.append(fillValue)
+
+    def __len__(self):
+        return len(self._items)
+
+    def __str__(self):
+        return str(self._items)
+
+    def __iter__(self):
+        return iter(self._items)
+
+    def __getitem__(self, index):
+        return self._items[index]
+
+    def __setitem__(self, index, newitem):
+        self._items[index] = newitem
+
+
+class Grid:
+    def __init__(self, rows, columns, fillValue=None):
+        self._data = Array(rows)
+        for row in range(rows):
+            self._data[row] = Array(columns, fillValue)
+
+    def getHeight(self):
+        return len(self._data)
+
+    def getWidth(self):
+        return len(self._data[0])
+
+    def __getitem__(self, index):
+        return self._data[index]
+
+    def __str__(self):
+        result = ""
+        for row in range(self.getHeight()):
+            for col in range(self.getWidth()):
+                result += str(self._data[row][col]) + " "
+            result += "\n"
+        return result
