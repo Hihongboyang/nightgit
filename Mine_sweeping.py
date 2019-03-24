76906f2482ca7172b3034d8bfdc840aec675f188
Hihongboyang <245035255@qq.com>
2019-03-24 23:00:04 +0800

    finish mine

--- Mine_sweeping.py
+++ Mine_sweeping.py
@@ -66,3 +66,11 @@ class Mine:
         self._status = value
 
     status = property(fget=get_status, fset=set_status,doc="状态")
+
+
+class MineBlock:
+    def __init__(self):
+        self._block = [Mine(i,j) for i in range(BLOCK_WIDTH) for j in range(BLOCK_HEIGHT)]
+
+        for i in random.sample(range(BLOCK_WIDTH * BLOCK_HEIGHT), MINE_COUNT):
+            self._block[i // BLOCK_WIDTH][i % BLOCK_WIDTH].value = 1
